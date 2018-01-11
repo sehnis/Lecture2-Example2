d330892e7649999a38c47d588fcee3b3d5734f3a
jczetta <jczetta@gmail.com>
2018-01-11 17:50:43 -0500

    Add initial file

---
+++ samepage.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def main_page():
+    return "Here's the main page. <a href='http://localhost:5000/seeform'>Click here to see the form</a>."
+
+@app.route('/seeform',methods=["GET","POST"])
+def see_form():
+    formstring = """<br><br>
+    <form action="http://localhost:5000/seeform" method='POST'>
+<input type="text" name="phrase"> Enter a phrase: <br>
+<input type="submit" value="Submit">
+"""
+    if request.method == "POST":
+        entered = request.form.get('phrase',"")
+        return "The last phrase entered was {}".format(entered) + formstring
+    else:
+        return formstring
+
+
+
+
+if __name__ == "__main__":
+    app.run(use_reloader=True, debug=True)
