6ba5b583aacb8df54990c6397bfe0dbaa7d0545e
sehnis <sehnis@umich.edu>
2018-01-12 12:00:30 -0500

    Added History feature

--- samepage.py
+++ samepage.py
@@ -6,21 +6,26 @@ app = Flask(__name__)
 def main_page():
     return "Here's the main page. <a href='http://localhost:5000/seeform'>Click here to see the form</a>."
 
+lastpost = ''
+lastget = ''
+
 @app.route('/seeform',methods=["GET","POST"])
 def see_form():
-    formstring = """<br><form action="/seeform" method='POST'> Enter a phrase to use the POST Method: <br>
-<input type="text" name="phrase"><br>
-<input type="submit" value="Submit"><br><br></form>
-
-<form action="/seeform" method='GET'> Enter a phrase to use the GET Method: <br>
-<input type="text" name="phrase"><br>
-<input type="submit" value="Submit"><br><br></form>
-""" ## HINT: In there ^ is where you need to add a little bit to the code...
     if request.method == "POST":
-        formstring += ("<br>POST USED<br>You entered: " + request.form['phrase'])
-        # Add more code here so that when someone enters a phrase, you see their data (somehow) AND the form!
+        global lastpost
+        lastpost = request.form['phrase']
+        # OR: request.form.get('phrase', '')
     if request.method == "GET":
-        	formstring += ("<br>GET USED<br>The data from the url is: " + request.args.get('phrase',''))
+        global lastget
+        lastget = request.args.get('phrase','')
+    
+    formone = """<br><form action="/seeform" method='POST'> Enter a phrase to use the POST Method: <br>
+<input type="text" name="phrase">&nbsp;Last POST request: <a style="color:green;">{}</a><br>
+<input type="submit" value="Submit"><br><br></form>""".format(lastpost)
+    formtwo = """<form action="/seeform" method='GET'> Enter a phrase to use the GET Method: <br>
+<input type="text" name="phrase">&nbsp;Last GET request: <a style="color:green;">{}</a><br>
+<input type="submit" value="Submit"><br><br></form>""".format(lastget)
+    formstring = formone + formtwo
     return formstring
 
 
