80e352310a996b3b0b591d2e58e249e93b1e058c
sehnis <sehnis@umich.edu>
2018-01-12 11:49:27 -0500

    Can now choose POST or GET

--- samepage.py
+++ samepage.py
@@ -8,16 +8,21 @@ def main_page():
 
 @app.route('/seeform',methods=["GET","POST"])
 def see_form():
-    formstring = """<br><br>
-    <form action="" method='POST'>
-<input type="text" name="phrase"> Enter a phrase: <br>
-<input type="submit" value="Submit">
+    formstring = """<br><form action="/seeform" method='POST'> Enter a phrase to use the POST Method: <br>
+<input type="text" name="phrase"><br>
+<input type="submit" value="Submit"><br><br></form>
+
+<form action="/seeform" method='GET'> Enter a phrase to use the GET Method: <br>
+<input type="text" name="phrase"><br>
+<input type="submit" value="Submit"><br><br></form>
 """ ## HINT: In there ^ is where you need to add a little bit to the code...
     if request.method == "POST":
-        pass
+        formstring += ("<br>POST USED<br>You entered: " + request.form['phrase'])
         # Add more code here so that when someone enters a phrase, you see their data (somehow) AND the form!
-    else:
-        return formstring
+    if request.method == "GET":
+        	formstring += ("<br>GET USED<br>The data from the url is: " + request.args.get('phrase',''))
+    return formstring
+
 
 
 
